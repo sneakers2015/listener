118517dbb3fd708871252f94208bd824b9b2a6a5
chulki kim <chulki7.kim@samsung.com>
2015-05-16 23:26:34 +0900

    add app.js
    add app.js

---
+++ js/app.js
@@ -0,0 +1,394 @@
+var listenerApp;
+
+(function(jQuery) {
+    jQuery.eventEmitter = {
+            _JQInit: function() {
+                this._JQ = jQuery(this);
+            },
+            emit: function(evt, data) {
+                !this._JQ && this._JQInit();
+                this._JQ.trigger(evt, data);
+            },
+            once: function(evt, handler) {
+                !this._JQ && this._JQInit();
+                this._JQ.one(evt, handler);
+            },
+            on: function(evt, handler) {
+                !this._JQ && this._JQInit();
+                this._JQ.bind(evt, handler);
+            },
+            off: function(evt, handler) {
+                !this._JQ && this._JQInit();
+                this._JQ.unbind(evt, handler);
+            }
+    };
+}(jQuery));
+
+/**
+ * Listener App model
+ * 
+ * Emit 'soundMatched' event when sound is matched.
+ * 
+ * @class
+ * @extends {jQuery.eventEmitter}
+ */
+function ListenerApp() {
+    this.sounds = {}; // soundID -> Sound map
+    this.currentState = 'stopped'; // 'stopped' | 'running'
+    this.history = []; // Alert list
+    this.settings = {showSoundIcons: true};
+    this.addNewSound = addNewSound;
+    this.deleteSound = deleteSound;
+}
+
+jQuery.extend(ListenerApp.prototype, jQuery.eventEmitter);
+
+function start() {
+    console.log('start');
+    listenerApp.currentState = 'running';
+}
+
+function stop() {
+    console.log('stop');
+    listenerApp.currentState = 'stopped';
+}
+
+/**
+ * Change sound properties
+ */
+function changeSettings(settingsObject) {
+    var settings = listenerApp.settings;
+    console.log('settingsObject', settings, settingsObject);
+    settingsObject = _.pick(settingsObject, 'showSoundIcons', 'runBackground');
+    _.extend(settings, settingsObject);
+    saveApp();
+    return true;
+}
+
+/**
+ * Sound model
+ * @param id
+ * @param title
+ * @param soundData
+ * @param samplePackage
+ * @param alertMethods
+ * @param dialNumber
+ * @param message
+ * @class
+ */
+function Sound(id, title, enabled, soundData, samplePackage, notiEnabled, dialNumber, message) {
+    this.id = id;
+    this.title = title;
+    this.enabled = enabled;
+    this.soundData = soundData;
+    this.samplePackage = samplePackage;
+    this.notiEnabled = notiEnabled;
+    this.dialNumber = dialNumber;
+    this.message = message;
+}
+
+/**
+ * Generate new sound id (max sound id + 1)
+ * @returns {Number}
+ */
+function generateNewSoundID() {
+    var maxid = 0;
+    for(var i in listenerApp.sounds) {
+        var sound = listenerApp.sounds[i];
+        maxid = (sound.id > maxid) ? sound.id : maxid; 
+    }
+    return maxid + 1;
+}
+
+/**
+ * Add new sound
+ * @param title
+ * @param soundData
+ * @returns {Sound}
+ */
+function addNewSound(title, soundData, samplePackage, dialNumber, message) {
+    console.log('addNewSound');
+    var newid = generateNewSoundID();
+    var _dialNumber = null;
+    var _message = null;
+    if (dialNumber !== undefined) {
+    	_dialNumber = dialNumber;
+    }
+    if (message !== undefined) {
+    	_message = message;
+    }
+    var newSound = new Sound(newid, title, true, soundData, samplePackage, true, _dialNumber, _message);  
+    listenerApp.sounds[newid] = newSound;
+    saveApp();
+    return newSound;
+}
+
+function getSoundByID(soundID) {
+    for (var i in listenerApp.sounds) {
+        var sound = listenerApp.sounds[i];
+        if (sound.id == soundID) {
+            return sound;
+        }
+    }
+    return null;
+}
+
+/**
+ * Change sound properties
+ */
+function changeSound(soundID, soundObject) {
+    var sound = getSoundByID(soundID);
+    console.log('changeSound', sound, soundObject);
+    if (!sound) {
+        console.error('changeSound soundID not found:', soundID);
+        return false;
+    }
+    soundObject = _.pick(soundObject, 'title', 'soundData', 'samplePackage', 'enabled', 'alertMethods', 'notiEnabled', '_dialNumber', '_message');
+    _.extend(sound, soundObject);
+    saveApp();
+    return true;
+}
+
+/**
+ * Delete sound
+ */
+function deleteSound(soundID) {
+    for (var i in listenerApp.sounds ) {
+        var sound = listenerApp.sounds[i];
+        if (sound.id == soundID) {
+            delete listenerApp.sounds[i];
+            saveApp();
+            return true;
+        }
+    }
+    console.error('changeSound soundID not found:', soundID);
+    return false;
+}
+
+/**
+ * load saved app data (sounds, settings, etc)
+ */
+function loadApp() {
+    console.log('load');
+    console.log('get appdata', localStorage.appdata);
+    console.log('before load listenerApp', listenerApp)
+    if (localStorage.appdata) {
+        _.extend(listenerApp, JSON.parse(localStorage.appdata));
+    }
+    console.log('after load listenerApp', listenerApp)
+}
+
+/**
+ * save app data (sounds, settings, etc)
+ * 
+ * call this when something that should be saved is changed
+ */
+function saveApp() {
+    console.log('save');
+    var appdata = _.pick(listenerApp, 'sounds', 'settings');
+    console.log('appdata', appdata);
+    localStorage.setItem('appdata', JSON.stringify(appdata));    
+    startMatching();
+}
+
+/**
+ * Initialize app
+ */
+function initApp() {
+    console.log('init');
+    listenerApp = new ListenerApp();
+    loadApp();
+//    init_Matcher();
+}
+
+window.onload = function () {
+    console.log('window onload');
+    initApp();
+};
+
+function startMatching() {
+//    var onSounds = _.filter(listenerApp.sounds, function (sound) { return sound.enabled; });
+//    var soundArray = _.toArray(onSounds);
+//    var samplePackages = _.pluck(soundArray, 'samplePackage');
+//    console.log('samplePackages', samplePackages);
+//    console.log("startMatching(packages, sampleMatched); length", samplePackages.length);
+//    matcher.startMatching(samplePackages, function (sampleIndex) {
+//        var sound = soundArray[sampleIndex];
+//        console.log("sample matched index:", sampleIndex, ", sound:", sound.id, sound.title);
+//        listenerApp.emit('soundMatched', sound.id);
+//    });
+}
+
+function stopMatching() {
+    console.log("stopMatching();");
+    matcher.stopMatching();
+}
+
+/**
+ * Initialize for Matcher
+ */
+var matcher;
+function init_Matcher() {
+    matcher = new Matcher();
+    startMatching();
+}
+
+/**
+ * Notification wrapper
+ * 
+ * notification {
+ *     id,
+ *  message,
+ *  vibration,
+ *  ledColor,
+ * }
+ */
+function notification(noti) {
+    try {
+        if ( isDevice() == true ) {
+            var notificationDict = {
+                    content : noti.message,
+                    iconPath : "../res/warning.png",
+                    soundPath : "",
+                    vibration : noti.vibration, // true,
+                    ledColor : noti.ledColor,   // "#FFFF00", 
+                    ledOnPeriod: 1000,
+                    ledOffPeriod : 500 };
+
+            var notification = new tizen.StatusNotification("SIMPLE", "Listener", notificationDict);
+            tizen.notification.post(notification);
+        } else {
+            var notification = '<div data-role="notification" id="'+ noti.id + '" data-type="ticker"><img src="../res/warning.png"><p>' + noti.message + '</p></div>';
+            $('#history').append(notification);
+
+            $('#'+noti.id).notification().on("click", function() { 
+                    $('#'+noti.id).remove();
+                    // FIXME: vibration and flash
+                    if ( noti.vibration == true ) {
+                        vibrate(false);
+                    }
+                });
+            $('#'+noti.id).notification('open');
+            if ( noti.vibration == true ) {
+                vibrate(true);
+            }
+        }
+    } catch (e) {
+        console.log (e.name + ": " + e.message);
+    }
+}
+
+/**
+ * Trigger for vibrate
+ * 
+ * @see http://www.w3.org/TR/2012/WD-vibration-20120202/
+ */
+var timeID = null;
+function vibrate(flag) {
+    if ( isDevice() == true ) {
+        if ( flag == true ) {
+            if ( timeID != null ) {
+                clearInterval(timeID);
+                timeID = null;
+            }
+            timeID = setInterval( function() { navigator.vibrate(1000); }, 1000);
+            console.log('device vibrate on');
+        } else {
+            if ( timeID != null ) {
+                clearInterval(timeID);
+                timeID = null;
+            }
+            navigator.vibrate(0);
+            console.log('device vibrate off');
+        }
+    } else {
+        if ( flag == true ) {
+            if ( timeID != null ) {
+                clearInterval(timeID);
+                timeID = null;
+            }
+            timeID = setInterval( function() { parent.require('ripple/ui/plugins/goodVibrations').shakeDevice(1); }, 500);
+            console.log('simulator vibrate on');
+        } else {
+            if ( timeID != null ) {
+                clearInterval(timeID);
+                timeID = null;
+            }
+            console.log('simulator vibrate off');
+        }
+    }
+}
+
+/**
+ * Trigger for flash
+ */
+var flashID = null;
+function flash(flag) {
+    if ( isDevice() == true ) {
+        // Nothing
+    } else {
+         if ( flag == true ) {
+             if ( flashID != null ) {
+                 clearInterval(flashID);
+                 flashID = null;
+             }
+             flashID = setInterval( function() { 
+                 setTimeout(function() {document.body.style.backgroundColor="#FF5050"; } ,1);
+                 setTimeout(function() {document.body.style.backgroundColor="#fff"; } ,500);
+             }, 800);
+             console.log('simulator flash on');
+         } else {
+             if ( flashID != null ) {
+                 clearInterval(flashID);
+                 flashID = null;
+             }
+             console.log('simulator flash off');
+         }
+     }
+}
+
+/**
+ * Check for target
+ */
+function isDevice() {
+    try {
+        if ( tizen instanceof Object ) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (e) {
+        return false;
+    }
+}
+
+/**
+ * Alert model
+ * @param soundID
+ * @param timestamp
+ * @class
+ */
+function Alert(soundID, timestamp) {
+    this.soundID = soundID;
+    this.timestamp = timestamp;
+}
+
+function History(soundID, timestamp) {
+    this.soundID = soundID;
+    this.timestamp = timestamp;
+}
+
+function addNewHistory(soundID, timestamp) {
+    var newHistory = new History(soundID, timestamp);
+    listenerApp.history.push(newHistory);
+}
+
+function getHistoryByID(soundID) {
+    for (var i in listenerApp.history ) {
+        var history = listenerApp.history[i];
+        if (history.soundID == soundID) {
+            return history;
+        }
+    }
+    return null;
+}
--- js/soundlist-controls.js
+++ js/soundlist-controls.js
@@ -1,16 +1,66 @@
 (function(){
 	
-	/*
-	 * page id = 'soundlist-page'
-	 * listview id = 'sound-listview'
-	 * delete popup id = 'sound-delete-popup'
-	 * delete pupup ok = 'sound-delete-pupup-ok'
-	 * delete pupup cancel = 'sound-delete-pupup-cancel'
-	 */
-	var page = document.getElementById( "soundlist-page" ),
-	listElement = page.getElementsByClassName( "ui-swipelist-list" )[0],
-	swipeList;
-    var deleteTargetElem = null;
+	var page = document.getElementById( "soundlist-page" );
+	var listElement = page.querySelector("#sound-listview" );
+	
+	var btn_addSound = page.querySelector( "#addSoundBtn" );
+	var soundDeletePopup = page.querySelector( "#sound-delete-popup" );
+	var soundDeletePopupBtnOk = page.querySelector( "#sound-delete-popup-ok" );
+	var soundDeletePopupBtnCancel = page.querySelector( "#sound-delete-popup-cancel" );
+	
+	var deleteTargetListItemId = null; //using
+	var swipeList = null //dynamic create
+	
+	function updateSoundList () {
+		var sound;
+		for (var i in listenerApp.sounds) {
+			sound = listenerApp.sounds[i];
+			addSoundListItem(sound.id, sound.title, sound.enabled, sound.dialNumber);
+		}
+	}
+	
+	function addSoundListItem (id, title, enabled, dialNumber) {
+		var li, label, span, input;
+		li = document.createElement('li')
+		label = document.createElement('label');
+		span = document.createElement('span');
+		input = document.createElement('input');
+		
+		li.setAttribute('id', id);
+		li.setAttribute('class', 'li-has-multiline li-has-checkbox')		
+		label.textContent = title;
+		
+		span.setAttribute('class', 'ui-li-sub-text li-text-sub');		
+		if (dialNumber !== undefined) {
+			span.textContent = dialNumber
+		} else {
+			span.textContent = '';
+		}
+		
+		input.setAttribute('type', 'checkbox');
+		if (enabled === true) {
+			input.checked = true;
+		} else {
+			input.checked = false;
+		}				
+		
+		label.appendChild(span);
+		label.appendChild(input);
+		li.appendChild(label)
+		listElement.appendChild(li);
+		console.log('list item added', li);
+	}
+	
+	function removeSoundListItem (id) {
+		var listItem = listElement.querySelector('#' + id);
+		if (listItem) {
+			console.log('list item deleted', listItem);
+			listItem.remove();			
+			return true;
+		}
+		return false;
+	}
+		   
 	page.addEventListener( "pagebeforeshow", function() {
 	// make SwipeList object
 		swipeList = tau.widget.SwipeList( listElement, {
@@ -25,29 +75,42 @@
 				
 		});
 					
-	listElement.addEventListener("swipelist.right", function(evt) { 		
+	listElement.addEventListener("swipelist.right", function(evt) {
+		console.log('swipe right', evt.target);
 		var list = evt.currentTarget;
-		tau.openPopup("#sound-delete-popup");		
+		tau.openPopup(soundDeletePopup);		
 		var target = evt.target; //list 항목
 		deleteTargetElem = target;
 //		target.remove(); //제거
 		var type = evt.type; //swipelist.left						    
 	});
+	
+	
+	listElement.addEventListener("swipelist.left", function(evt) {
+		console.log('swipe left', evt.target);
+		var list = evt.currentTarget;
+//		tau.openPopup(soundDeletePopup);		
+//		var target = evt.target; //list 항목
+//		deleteTargetElem = target;	
+//		var type = evt.type; //swipelist.left						    
+	});
+	
 
 	});
+	
 	document.getElementById('sound-delete-popup-cancel').addEventListener('click', function(ev)
-			   {
-			      tau.closePopup();
-			   });
+	{
+	     tau.closePopup();
+	});
 	
 	document.getElementById('sound-delete-popup-ok').addEventListener('click', function(ev)
-			   {
-					if (deleteTargetElem !== null) {
-						deleteTargetElem.remove();
-						deleteTargetElem = null;
-					}							
-					tau.closePopup();
-			   });
+	{
+		if (deleteTargetElem !== null) {			
+			deleteTargetElem.remove();
+			deleteTargetElem = null;
+		}							
+		tau.closePopup();
+	});
 
 
 
@@ -87,13 +150,14 @@
 	}
 
 	document.getElementById('addSoundBtn').addEventListener('click', function(ev)
-			   {
-			      addListItem('응애응애', true, 111);
-			      addListItem('헬프미', false, 222);
-			   });
-	//swipe end
-
-	
-	//swipe end
+	{
+		addNewSound('test01', null, null, 111, 'test01');
+		addNewSound('test02', null, null, 112, 'test02');
+		addNewSound('test03', null, null, 113, 'test03');
+		updateSoundList();
+		
+//			      addListItem('응애응애', true, 111);
+//			      addListItem('헬프미', false, 222);
+	});	
 
 }());
