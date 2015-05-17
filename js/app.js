78e00c734ec37f223b04fcfece1a28d769ef3151
chulki kim <chulki7.kim@samsung.com>
2015-05-18 00:23:29 +0900

    bind 'tizenkwkey' event
    .

--- js/app.js
+++ js/app.js
@@ -344,3 +344,18 @@ function getHistoryByID(soundID) {
     }
     return null;
 }
+
+window.addEventListener( 'tizenhwkey', function( ev ) {
+	if( ev.keyName === "back" ) {
+		var page = document.getElementsByClassName( 'ui-page-active' )[0],
+			pageid = page ? page.id : "";
+//		if( pageid === "main" ) {
+//			try {
+//				tizen.application.getCurrentApplication().exit();
+//			} catch (ignore) {
+//			}
+//		} else {
+			window.history.back();
+//		}
+	}
+} );
